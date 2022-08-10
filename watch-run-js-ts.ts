dfc8d25ac631da833d3d68b50de7c58ed3daf3a5
hisland <hisland@qq.com>
2022-08-10 10:19:17 +0800

    init

---
+++ watch-run-js-ts.ts
@@ -0,0 +1,71 @@
+// deno run -A z-deno-watch-reload.js
+
+import * as Colors from 'https://deno.land/std@0.151.0/fmt/colors.ts'
+import { dirname, basename, relative } from 'https://deno.land/std@0.151.0/path/mod.ts'
+
+const __filepath = new URL('', import.meta.url).pathname
+// console.log('__filepath: ', __filepath)
+const __filename = basename(__filepath)
+// console.log('__filename: ', __filename)
+const __dirname = dirname(__filepath)
+// console.log('__dirname: ', __dirname)
+
+let COUNT = 1
+
+const handle = debounce((event: unknown) => {
+  const {
+    kind,
+    paths: [path],
+  } = event as Deno.FsEvent
+  // console.log('event: ', event)
+  // console.log('kind: ', kind)
+  // console.log('path: ', path)
+  if (!["create", "modify"].includes(kind)) return
+  if (!/\.[jt]s/.test(path)) return
+
+  const filename = basename(path)
+  // console.log('filename: ', filename)
+  const relative_file = relative(__dirname, path)
+  // console.log('relative_file: ', relative_file)
+  try {
+    Deno.lstatSync(relative_file);
+  } catch (err) {
+    if (err instanceof Deno.errors.NotFound) {
+      return false;
+    }
+  }
+
+  console.log(`\n${Colors.red('[' + (COUNT++) + '] reload')} ${relative_file}: \n`)
+  Deno.run({
+    cmd: [
+      `deno`,
+      `run`,
+      `-A`,
+      `${relative_file}`,
+    ],
+  })
+}, 100)
+
+async function run() {
+  const watcher = Deno.watchFs('.')
+  for await (const event of watcher) {
+    handle(event)
+  }
+}
+
+console.log('start')
+run()
+
+function debounce(func: (...args: unknown[]) => unknown, wait: number, fireAtHead = false) {
+  let timeout: number | undefined
+  return (...args: unknown[]) => {
+    const later = () => {
+      timeout = undefined
+      if (!fireAtHead) func(...args)
+    }
+    const needCall = fireAtHead && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (needCall) func(...args)
+  }
+}
